6447a59a1101eb491ae569ce26dfeb1ff6d73cba
Vira05 <117241657+Vira05@users.noreply.github.com>
2022-11-06 15:14:13 -0500

    TestH2TagText_WhenSearchingCityCountry (#17)
    Co-authored-by: Vira Gavrylyshyna <vira@Viras-MacBook-Air.local>

---
+++ src/test/java/Vira05Test.java
@@ -0,0 +1,51 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class Vira05Test extends BaseTest {
+
+    @Test
+    public void testH2TagText_WhenSearchingCityCountry() throws InterruptedException {
+
+
+        String url = "https://openweathermap.org/";
+        String cityName = "Paris";
+        String expectedResult = "Paris, FR";
+
+        getDriver().get(url);
+
+        Thread.sleep(6000);
+
+        WebElement searchCityFild = getDriver().findElement(
+                By.xpath("//div[@id = 'weather-widget']//input[@placeholder='Search city']")
+        );
+        searchCityFild.click();
+        searchCityFild.sendKeys(cityName);
+
+        WebElement searchButton = getDriver().findElement(
+                By.xpath("//button[@type ='submit']")
+        );
+        searchButton.click();
+        Thread.sleep(1000);
+
+        WebElement parisFRChoiceInDropDownMenu  = getDriver().findElement(
+                By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']")
+        );
+        parisFRChoiceInDropDownMenu.click();
+        Thread.sleep(1000);
+
+
+
+        WebElement h2CityCountryHeader = getDriver().findElement(
+                By.xpath("//div[@id ='weather-widget']//h2")
+        );
+        Thread.sleep(2000);
+
+
+        String actualResult = h2CityCountryHeader.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+}
