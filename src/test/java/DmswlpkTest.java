5e0d69c45416dab4b6d186da4b9e65e48db25b17
Tatiana P <103320248+dmswlpk@users.noreply.github.com>
2022-11-07 13:09:36 -0600

    Test11_09 (#75)

--- src/test/java/DmswlpkTest.java
+++ src/test/java/DmswlpkTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -7,6 +9,7 @@ import runner.BaseTest;
 import java.util.List;
 
 import static java.lang.Thread.sleep;
+import static org.openqa.selenium.Keys.ENTER;
 
 public class DmswlpkTest extends BaseTest {
     @Test
@@ -32,5 +35,22 @@ public class DmswlpkTest extends BaseTest {
         }
 
         Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testTC_11_09() throws InterruptedException {
+        String url = "https://openweathermap.org/";
+
+        getDriver().get(url);
+
+        sleep(4000);
+
+        getDriver().findElement(
+                By.name("q")).sendKeys("Rome", ENTER);
+
+        Assert.assertTrue(getDriver().getCurrentUrl().contains("Rome"));
+        Assert.assertTrue(getDriver().getCurrentUrl().contains("find"));
+        Assert.assertTrue(getDriver().findElement(By.xpath("//input[@id='search_str']")).getText()
+                .equals(getDriver().findElement(By.xpath("//input[@value='Rome']")).getText()));
     }
 }
