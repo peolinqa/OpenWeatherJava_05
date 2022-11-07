2bb63a4c42c0bf70792c7cfaee578d2089b059bd
ktitova <ksunchik123@yandex.ru>
2022-11-07 20:12:27 +0300

    add testConfirmTemperatureFaringate - 2

--- src/test/java/KsutitovaTest.java
+++ src/test/java/KsutitovaTest.java
@@ -1,7 +1,5 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
