d9fd52e2712ce0cd0941dc538b7b050765ee2213
Elena_S <sosnitsky.e@gmail.com>
2022-11-07 15:30:52 +0200

    testOpenWeatherPageGuidMaximixe() throws added (#62)

--- src/test/java/Elena_STest.java
+++ src/test/java/Elena_STest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -40,6 +42,26 @@ public class Elena_STest extends BaseTest {
 
         Assert.assertEquals(actualResult,expectedResult);
 
+    }
+
+    @Test
+    public void testOpenWeatherPageGuidMaximixe() throws InterruptedException {
+
+        String url = "https://openweathermap.org/";
+        String expectedURL = "https://openweathermap.org/guide";
+        String expectedResult = "OpenWeatherMap API guide - OpenWeatherMap";
+        getDriver().get(url);
+//        getDriver().manage().window().maximize(); //Maximize current window
+        WebElement searchButtonGUid = getDriver().findElement(
+                By.xpath("//div[@id='desktop-menu']/ul/li/a[@href='/guide']")
+        );
+
+        Thread.sleep(5000);
+        searchButtonGUid.click();
+        String actualResultURL = getDriver().getCurrentUrl();
+        String actualResult = getDriver().getTitle();
+        Assert.assertEquals(actualResultURL,expectedURL);
+        Assert.assertEquals(actualResult,expectedResult);
 
     }
 }
