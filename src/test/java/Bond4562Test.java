52fa68b80581f424ea0f37a956f8846af346e4c8
bond4562 <107379997+bond4562@users.noreply.github.com>
2022-11-08 17:35:00 +0700

    testClickGuideAndVerifyTitleandUrl() added (#119)

---
+++ src/test/java/Bond4562Test.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class Bond4562Test extends BaseTest {
+
+    @Test
+    public void testClickGuideAndVerifyTitleandUrl() throws InterruptedException {
+
+        String urlBase = "https://openweathermap.org/";
+        String expectedResultUrl = "https://openweathermap.org/guide";
+        String expectedtResultTitle = "OpenWeatherMap API guide - OpenWeatherMap";
+
+
+        getDriver().get(urlBase);
+
+        Thread.sleep(5000);
+
+
+        getDriver().findElement(
+                By.xpath("//nav[@id='nav-website']//div[@id='desktop-menu']//a[@href='/guide']")
+        ).click();
+
+        String actualResultUrl = getDriver().getCurrentUrl();
+        String actualResultTitle = getDriver().getTitle();
+
+
+        Assert.assertEquals(actualResultUrl, expectedResultUrl);
+        Assert.assertEquals(actualResultTitle, expectedtResultTitle);
+    }
+
+}
