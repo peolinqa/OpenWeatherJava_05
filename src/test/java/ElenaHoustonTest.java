f434843de515cadeecd0e6339c993faaf351afe9
ElenaHouston <115054183+ElenaHouston@users.noreply.github.com>
2022-11-07 19:41:16 -0600

    ElenaHouston (#82)

--- src/test/java/ElenaHoustonTest.java
+++ src/test/java/ElenaHoustonTest.java
@@ -5,10 +5,9 @@ import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class ElenaHoustonTest extends BaseTest {
+
     @Test
-
     public void testH2searchWeather_CityCountry () throws InterruptedException {
-
 
         String url = "https://openweathermap.org/";
         String cityName = "Paris";
@@ -47,8 +46,26 @@ public class ElenaHoustonTest extends BaseTest {
         String actualResult = h2CityCountryHeader.getText();
 
         Assert.assertEquals(actualResult, expectedResult);
-
-    }
     }
 
+    @Test
+    public void testConfirmTheLinkAndTitleOnThePage_OpenWeatherMap() throws InterruptedException {
+        String url = "https://openweathermap.org/";
+        String guideMenuText = "Guide";
+        String expectedResult1 = "https://openweathermap.org/guide";
 
+        String expectedResult2 = "OpenWeatherMap API guide - OpenWeatherMap";
+
+        getDriver().get(url);
+        Thread.sleep(5000);
+
+        WebElement guideMenu = getDriver().findElement(By.linkText(guideMenuText));
+        guideMenu.click();
+
+        String actualResult1 = getDriver().getCurrentUrl();
+        String actualResult2 = getDriver().getTitle();
+
+        Assert.assertEquals(actualResult1, expectedResult1);
+        Assert.assertEquals(actualResult2, expectedResult2);
+    }
+}
