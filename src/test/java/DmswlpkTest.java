f30f2bfa6f81a8ecee895dc0b509fdd3d0be816c
Tatiana P <103320248+dmswlpk@users.noreply.github.com>
2022-11-08 15:06:06 -0600

    added TC_11_03 and deleted unnecessary imports (#161)

--- src/test/java/DmswlpkTest.java
+++ src/test/java/DmswlpkTest.java
@@ -53,4 +53,35 @@ public class DmswlpkTest extends BaseTest {
         Assert.assertTrue(getDriver().findElement(By.xpath("//input[@id='search_str']")).getText()
                 .equals(getDriver().findElement(By.xpath("//input[@value='Rome']")).getText()));
     }
+
+    @Test
+    public void testTC_11_03() throws InterruptedException {
+
+        String url = "https://openweathermap.org/";
+
+        getDriver().get(url);
+
+        sleep(4000);
+
+        String expectedResult = "We use cookies which are essential for the site to work. " +
+                "We also use non-essential cookies to help us improve our services. Any data collected is anonymised." +
+                " You can allow all cookies or manage them individually.";
+
+        WebElement textAtBottom = getDriver().findElement(By.xpath("//div[@class='stick-footer-panel__container']/p"));
+
+        WebElement allAllowButton = getDriver().findElement(By.xpath("//div[@class='container']//button"));
+
+        WebElement manageCookiesButton = getDriver().findElement(By.xpath("//a[@href='/cookies-settings']"));
+
+        Assert.assertEquals(textAtBottom.getText(), expectedResult);
+        Assert.assertTrue(allAllowButton.isDisplayed());
+        Assert.assertEquals(allAllowButton.getText(), "Allow all");
+        Assert.assertTrue(manageCookiesButton.isEnabled());
+
+        manageCookiesButton.click();
+
+        sleep(7000);
+
+        Assert.assertNotEquals(getDriver().getCurrentUrl(), url);
+    }
 }
