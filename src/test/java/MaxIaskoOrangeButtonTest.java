55b746bc07d0868dcd10e6a93fd66f87d0598587
Max Iasko <115347581+maksymiasko@users.noreply.github.com>
2022-11-08 14:05:45 +0000

    ATMaxIasko (#131)
    * testSearch30OrangeButtons added
    
    * made some changes (#88)
    
    Co-authored-by: Liudmila Plucci <112190395+liuda33@users.noreply.github.com>
    
    * Moved test (#96)
    
    * testSupportDropDownMenu() added (#98)
    
    * testH2TagText_WhenSearchingCityCountry() added (#92)
    
    * testH2TagText_WhenSearchingCityCountry() added (#94)
    
    * ElenaHouston (#82)
    
    * testButtonGuide_OpenWeather()
    
    * testError_WhenSubmitInSupportWithoutCaptcha() (#103)
    
    * PratasevichSerge (#104)
    
    * testH2TagText_WhenSearchingCityCountry() added
    
    * ElenaHouston (#106)
    
    * added
    
    * testUnitsOfMeasurement_ImperialFMphConfirmFLondon() added
    
    Co-authored-by: Sergey Kuznetsov <votkinsk@gmail.com>
    
    * testH2TextWhenSearchingCityCountry() (#102)
    
    * testH2TagText_WhenSearchingCityCountry() added
    
    * ATLarisaBratukhina (#117)
    
    * testH2TagText_WhenSearchingCityCountry() added
    
    * Fixed CI fails
    
    * VadimKachanovich1 (#85)
    
    * WhenSearchingCityCountry() added
    
    * method name starts with test(lower case)
    
    * testH2TagText_WhenSearchingCityCountry() added (#120)
    
    * testH2TagText_WhenSearchingCityCountry added (#122)
    
    * testH2TagText_WhenSearchingCityCountry() added (#123)
    
    * testTemperatureFormatSelectionInFahrenheit added (#118)
    
    * testOpenWeatherMapInMenuWhenPressButton()IrinaDud (#121)
    
    testOpenWeatherMapInMenuWhenPressButton()IrinaDud (#121)
    
    * testH2TagText_WhenSearchingCityCountry() added (#124)
    
    * testOpenWeatherPageGuidMaximize() added (#126)
    
    * testClickGuideAndVerifyTitleandUrl() added (#119)
    
    * testSearch30OrangeButtons added
    
    * testSearch30OrangeButtons added
    
    * testSearch30OrangeButtons added
    
    * testSearch30OrangeButtons added waiting time
    
    Co-authored-by: Liudmila Plucci <112190395+LiudmilaPlucci@users.noreply.github.com>
    Co-authored-by: Liudmila Plucci <112190395+liuda33@users.noreply.github.com>
    Co-authored-by: Roman T <93357374+Rom-T@users.noreply.github.com>
    Co-authored-by: Viktoriya D <115054456+ViktoriyaED@users.noreply.github.com>
    Co-authored-by: Marianna <105119046+MariannaLiss@users.noreply.github.com>
    Co-authored-by: Anastassia-I <115176063+Anastassia-I@users.noreply.github.com>
    Co-authored-by: ElenaHouston <115054183+ElenaHouston@users.noreply.github.com>
    Co-authored-by: Dashechka-blond <d.shevchenka@gmail.com>
    Co-authored-by: Olga-Khliupina <115127136+Olga-Khliupina@users.noreply.github.com>
    Co-authored-by: Serge Pratasevich <116032098+PratasevichSerge@users.noreply.github.com>
    Co-authored-by: Sergey Kuznetsov <votkinsk@gmail.com>
    Co-authored-by: BugorDmitriy <116024901+BugorDmitriy@users.noreply.github.com>
    Co-authored-by: Natalia1903 <98782051+Natalia1903@users.noreply.github.com>
    Co-authored-by: Lara Bee <115852420+larisabratukhina@users.noreply.github.com>
    Co-authored-by: Vadim Kachanovich <vad.kachanovich@gmail.com>
    Co-authored-by: Viacheslav Anisimov <forvsn@gmail.com>
    Co-authored-by: AnnaAbg <106620445+AnnaAbg@users.noreply.github.com>
    Co-authored-by: marinarokhmanova <115847672+marinarokhmanova@users.noreply.github.com>
    Co-authored-by: Elena_S <sosnitsky.e@gmail.com>
    Co-authored-by: Irina Dudchenko <110687018+IrinaDud@users.noreply.github.com>
    Co-authored-by: Andrey Golov <andrey.golov@yahoo.com>
    Co-authored-by: bond4562 <107379997+bond4562@users.noreply.github.com>

---
+++ src/test/java/MaxIaskoOrangeButtonTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class MaxIaskoOrangeButtonTest extends BaseTest {
+
+    @Test
+    public void testSearch30OrangeButtons() throws InterruptedException {
+        String url = "http://openweathermap.org/";
+
+        getDriver().get(url);
+        int expectedResult = 30;
+
+        Thread.sleep(7000);
+        WebElement searchApiPage = getDriver().findElement(
+                By.xpath("//div[@id = 'desktop-menu']//a[text()= 'API']"));
+        searchApiPage.click();
+
+        int actualResult = getDriver().findElements(
+                By.xpath("//a[contains(@class, 'btn_block orange round') " +
+                        "or contains(@class, 'ow-btn round btn-orange')]")).size();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+}
+
