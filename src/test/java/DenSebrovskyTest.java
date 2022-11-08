feae7750694ccb9e5a34528dfaa6e394835c2f46
Denis Sebrovsky <den.sebrovsky@mail.ru>
2022-11-08 17:25:25 +0300

    added DenSebrovskyTest (#132)
    added testFindThirtyOrangeButtons ()

---
+++ src/test/java/DenSebrovskyTest.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.util.List;
+
+public class DenSebrovskyTest extends BaseTest {
+
+    @Test
+    public void testFindThirtyOrangeButtons () throws InterruptedException {
+        int expectedresult = 30;
+
+        getDriver().get("https://openweathermap.org/");
+        Thread.sleep(5000);
+        getDriver().findElement(By.xpath(
+                "//nav[@id = 'nav-website']//div[@id = 'desktop-menu']//a[@href = '/api']")).click();
+        List<WebElement> orangeButtonsList = getDriver().findElements(By.xpath(
+                "//a[contains(@class, 'orange')]"));
+
+        Assert.assertEquals(expectedresult, orangeButtonsList.size());
+    }
+}
