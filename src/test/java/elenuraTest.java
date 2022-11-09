c926471d42f429663d13efa607fa1c60adb2d9f0
elenura <114898601+elenura@users.noreply.github.com>
2022-11-08 20:41:29 -0600

    testTitleOpenWeatherMapAPIGuide() added (#159)
    Co-authored-by: Irina Z Java <66705803+ArinaJur@users.noreply.github.com>

--- src/test/java/elenuraTest.java
+++ src/test/java/elenuraTest.java
@@ -3,9 +3,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
-
 public class elenuraTest extends BaseTest {
-
     @Test
     public void testH2TagTextWhenSearchingCityCountry() throws InterruptedException {
 
@@ -26,6 +24,8 @@ public class elenuraTest extends BaseTest {
                 By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']")
         );
         searchButton.click();
+        Thread.sleep(1000);
+
 
         WebElement parisFRChoiceInDropdownMenu = getDriver().findElement(
                 By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']")
@@ -35,18 +35,32 @@ public class elenuraTest extends BaseTest {
         WebElement h2CityCountryHeader = getDriver().findElement(
                 By.xpath("//div[@id = 'weather-widget']//h2")
         );
-
         Thread.sleep(5000);
         String actualResult = h2CityCountryHeader.getText();
-
         Assert.assertEquals(actualResult, expectedResult);
 
-
-//        Thread.sleep(5000);
-
         getDriver().quit();
-
     }
 
+    @Test
+
+    public void testTitleOpenWeatherMapAPIGuide() throws InterruptedException {
+
+        String url = "https://openweathermap.org/";
+        String expectedResultOpenPage = "https://openweathermap.org/guide";
+        String expectedResultTitle = "OpenWeatherMap API guide - OpenWeatherMap";
+
+        getDriver().get(url);
+        Thread.sleep(5000);
+
+        WebElement Title = getDriver().findElement(
+                By.xpath("//div/ul/li/a[@href='/guide']")
+        );
+        Title.click();
+        Thread.sleep(3000);
+
+        Assert.assertEquals(getDriver().getCurrentUrl(),expectedResultOpenPage);
+        Assert.assertEquals(getDriver().getTitle(),expectedResultTitle);
+    }
 
 }
