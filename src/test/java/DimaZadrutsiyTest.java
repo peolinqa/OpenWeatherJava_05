125b6630b5f441f892ec18bbd3e2ea81cc665112
Irina Z Java <tester@test.com>
2022-11-08 21:54:00 -0500

    testPageReload(DimaZadrutsiyTest) ignored

--- src/test/java/DimaZadrutsiyTest.java
+++ src/test/java/DimaZadrutsiyTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -41,6 +42,7 @@ public class DimaZadrutsiyTest extends BaseTest {
         Assert.assertEquals(actualResult, expectedResult);
     }
 
+    @Ignore
     @Test
     public void testPageReload() throws InterruptedException {
 
